0fa04d311d4600532653ae9884871bf36f4f7b9d
sl354207 <sl354207@ohio.edu>
2021-03-25 15:58:08 -0400

    connected mongodb database

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import { connectToDatabase } from '../util/mongodb'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ isConnected }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +14,15 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        {isConnected ? (
+          <h2>You are connected to MongoDB</h2>
+        ) : (
+          <h2>
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -62,4 +72,14 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { client } = await connectToDatabase()
+
+  const isConnected = await client.isConnected()
+
+  return {
+    props: { isConnected },
+  }
 }
