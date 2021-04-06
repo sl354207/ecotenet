5ca0d5855d2d16a87be1f6cc5c919f3e50d41697
sl354207 <sl354207@ohio.edu>
2021-04-06 11:19:34 -0400

    added error handling to get api

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,9 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { getMovies } from '../../utils/mongodb'
 
-export default async (req, res) => {
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405);
+  }
+  try {
+    const movies = await getMovies();
+  return res.status(200).json(movies);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: 'something went wrong'});
+  }
   
-  const movies = await getMovies();
-  return res.json(movies);
   
 };
