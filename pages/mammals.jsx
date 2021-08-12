32bc7c619e3fae1e10406b55c5cdba1b0735cde8
sl354207 <sl354207@ohio.edu>
2021-08-12 16:32:35 -0400

    added sticky list header with scrollto functionality

--- pages/mammals.jsx
+++ pages/mammals.jsx
@@ -2,37 +2,94 @@ import { getMammals } from "../utils/mongodb";
 
 import Link from "next/link";
 
-import { Button, IconButton } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListSubheader,
+  ListItemText,
+  AppBar,
+  Toolbar,
+} from "@material-ui/core";
+
+import { useRef, createRef } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  subheader: {
+    display: "flex",
+    // position: "sticky",
+    // width: "100%",
+    // maxWidth: 36,
+    // backgroundColor: theme.palette.secondary.main,
+  },
+  list: {
+    // display: "flex",
+    // width: "100%",
+    // maxWidth: 36,
+  },
+}));
 
 const mammals = ({ mammals }) => {
+  const classes = useStyles();
+
   const uniqueFirst = [
     ...new Set(mammals.map((mammal) => mammal.Scientific_Name[0])),
   ];
-  console.log(uniqueFirst);
+  // console.log(uniqueFirst);
+  const refs = uniqueFirst.reduce((acc, value) => {
+    acc[value] = useRef();
+    return acc;
+  }, {});
+  // console.log(refs);
 
+  const handleClick = (id) =>
+    refs[id].current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   return (
     <>
-      <ul>
+      <AppBar position="sticky">
+        <Toolbar>
+          <List component="div" className={classes.subheader}>
+            {uniqueFirst.map((item) => (
+              <ListItem
+                className={classes.list}
+                key={item}
+                button
+                onClick={() => handleClick(item)}
+              >
+                <ListItemText>{item}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Toolbar>
+      </AppBar>
+
+      <List>
         {uniqueFirst.map((entry) => {
           return (
             <>
-              <li>
+              <ListItem ref={refs[entry]}>
                 <a href="/mammal">{entry}</a>
-              </li>
+              </ListItem>
               {mammals.map((mammal) => {
                 if (mammal.Scientific_Name[0] === entry) {
                   return (
-                    <li>
+                    <ListItem>
                       <a href="/mammal">{mammal.Scientific_Name}</a>
-                    </li>
+                    </ListItem>
                   );
                 }
               })}
             </>
           );
         })}
-      </ul>
-      <Link href="/posts">Go Back</Link>
+      </List>
+      <Link href="/posts" id="back-to-top-anchor">
+        Go Back
+      </Link>
     </>
   );
 };
