33629dab91b0e1f3d956f93b5d1e6e9be0b742b9
sl354207 <sl354207@ohio.edu>
2021-10-15 14:14:47 -0400

    Initial commit for app search functionality

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -410,3 +410,143 @@ const Nav = () => {
 };
 
 export default Nav;
+
+// import React from "react";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import IconButton from "@material-ui/core/IconButton";
+// import Typography from "@material-ui/core/Typography";
+// import InputBase from "@material-ui/core/InputBase";
+// import Popper from "@material-ui/core/Popper";
+// import { alpha, makeStyles } from "@material-ui/core/styles";
+// import MenuIcon from "@material-ui/icons/Menu";
+// import SearchIcon from "@material-ui/icons/Search";
+// import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+
+// // CLICK AWAY LISTENER
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2)
+//   },
+//   title: {
+//     flexGrow: 1,
+//     display: "none",
+//     [theme.breakpoints.up("sm")]: {
+//       display: "block"
+//     }
+//   },
+//   search: {
+//     position: "relative",
+//     borderRadius: theme.shape.borderRadius,
+//     backgroundColor: alpha(theme.palette.common.white, 0.15),
+//     "&:hover": {
+//       backgroundColor: alpha(theme.palette.common.white, 0.25)
+//     },
+//     marginLeft: 0,
+//     width: "100%",
+//     [theme.breakpoints.up("sm")]: {
+//       marginLeft: theme.spacing(1),
+//       width: "auto"
+//     }
+//   },
+//   searchIcon: {
+//     padding: theme.spacing(0, 2),
+//     height: "100%",
+//     position: "absolute",
+//     pointerEvents: "none",
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   },
+//   inputRoot: {
+//     color: "inherit"
+//   },
+//   inputInput: {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+//     transition: theme.transitions.create("width"),
+//     width: "100%",
+//     [theme.breakpoints.up("sm")]: {
+//       width: "12ch",
+//       "&:focus": {
+//         width: "20ch"
+//       }
+//     }
+//   },
+//   paper: {
+//     border: "1px solid",
+//     padding: theme.spacing(1),
+//     backgroundColor: theme.palette.background.paper
+//   }
+// }));
+
+// export default function SearchAppBar() {
+//   const classes = useStyles();
+//   const [anchorEl, setAnchorEl] = React.useState(null);
+//   const [value, setValue] = React.useState("");
+//   const [open, setOpen] = React.useState(false);
+
+//   const handleClick = (event) => {
+//     setValue(event.target.value);
+//     console.log(event.target.value);
+//     if (event.target.value === "") {
+//       setOpen(false);
+//       setAnchorEl(null);
+//     } else {
+//       setOpen(true);
+//       setAnchorEl(event.currentTarget);
+//     }
+//   };
+
+//   const handleClickAway = () => {
+//     setOpen(false);
+//   };
+
+//   // const open = Boolean(anchorEl);
+//   const id = open ? "simple-popper" : undefined;
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <IconButton
+//             edge="start"
+//             className={classes.menuButton}
+//             color="inherit"
+//             aria-label="open drawer"
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography className={classes.title} variant="h6" noWrap>
+//             Material-UI
+//           </Typography>
+//           <ClickAwayListener onClickAway={handleClickAway}>
+//             <div className={classes.search}>
+//               <div className={classes.searchIcon}>
+//                 <SearchIcon />
+//               </div>
+//               <InputBase
+//                 placeholder="Search…"
+//                 classes={{
+//                   root: classes.inputRoot,
+//                   input: classes.inputInput
+//                 }}
+//                 inputProps={{ "aria-label": "search" }}
+//                 onFocus={() => setValue("")}
+//                 onChange={handleClick}
+//                 value={value}
+//               />
+//               <Popper id={id} open={open} anchorEl={anchorEl}>
+//                 <div className={classes.paper}>Search for '{value}'</div>
+//               </Popper>
+//             </div>
+//           </ClickAwayListener>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
