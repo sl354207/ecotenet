a892c62589fa1c52518e4f1c49ad76c04ae02d65
sl354207 <sl354207@ohio.edu>
2022-12-24 12:27:17 -0500

    Initial mongo testing setup

--- __tests__/utils/mongodb/mongoHelpers.test.js
+++ __tests__/utils/mongodb/mongoHelpers.test.js
@@ -1,8 +1,11 @@
 // EXAMPLE MONGO TEST
 
+import { connectToDatabase, createComment } from "@utils/mongodb/mongoHelpers";
 import { MongoClient } from "mongodb";
-// import { connectToDatabase } from "@utils/mongodb/mongoHelpers";
+jest.mock("@utils/mongodb/mongoHelpers");
 // import { client } from "@utils/mongodb/mongoPromise";
+
+// connectToDatabase = jest.fn();
 
 describe("insert", () => {
   let connection;
@@ -16,7 +19,17 @@ describe("insert", () => {
     // console.log(connection);
     // const client = await clientPromise;
     // db = await connectToDatabase();
-    db = await connection.db(globalThis.__MONGO_DB_NAME__);
+    // db = await connection.db(globalThis.__MONGO_DB_NAME__);
+    connectToDatabase.mockImplementation(async () => {
+      db = await connection.db(globalThis.__MONGO_DB_NAME__);
+      return Promise.resolve(db);
+    });
+
+    await connectToDatabase();
+
+    // connectToDatabase = jest.fn();
+    // connectToDatabase.mockResolvedValue("test");
+
     // console.log(db);
   });
 
@@ -26,16 +39,48 @@ describe("insert", () => {
   });
 
   it("should insert a doc into collection", async () => {
-    const users = db.collection("users");
-    const mockUser = { _id: "some-user-id", name: "John" };
+    // console.log(db);
+    // const users = db.collection("users");
+    const comments = db.collection("comments");
+    // const mockUser = { _id: "some-user-id", name: "John" };
+    const mockComment = {
+      _id: "test comment id",
+      name: "test name",
+      post_id: "test_id",
+      comment_ref: "test ref",
+      date: new Date().toUTCString(),
+      text: "test comment",
+      approved: "pending",
+      updated: false,
+    };
+
+    createComment.mockImplementation(async (mockComment) => {
+      const data = {
+        ...mockComment,
+      };
+      // console.log(db);
+      const response = await db.collection("comments").insertOne(data);
+
+      return Promise.resolve(response);
+    });
+    // connectToDatabase.mockResolvedValueOnce("test");
 
     try {
-      await users.insertOne(mockUser);
+      // await users.insertOne(mockUser);
+      await createComment(mockComment);
+      // await createComment(mockComment);
+      // console.log(createComment.mock.results[0]);
       // await users.insertOne(mockUser); // duplicate key error
     } catch (error) {
       throw new Error(error);
     }
-    const insertedUser = await users.findOne({ _id: "some-user-id" });
-    expect(insertedUser).toEqual(mockUser);
+
+    const insertedComment = await comments.findOne({ _id: "test comment id" });
+    // console.log(db);
+    // console.log(insertedComment);
+    expect(insertedComment).toEqual(mockComment);
+    // const insertedUser = await users.findOne({ _id: "some-user-id" });
+    // expect(insertedUser).toEqual(mockUser);
+    // expect(connectToDatabase).toHaveBeenCalledTimes(1);
   });
 });
