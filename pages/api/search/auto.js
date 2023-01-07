18b2f947a018f00ea7b4e913e2906470a6445b99
sl354207 <sl354207@ohio.edu>
2023-01-07 11:27:13 -0500

    Update eco, search, and vote validation

--- pages/api/search/auto.js
+++ pages/api/search/auto.js
@@ -7,15 +7,19 @@ export default async function handler(req, res) {
 
   // set id based on id of url query
   const query = req.query.q;
+  if (typeof query == "string") {
+    try {
+      const results = await autoSpecies(query);
+
+      return res.status(200).json(results);
+    } catch (err) {
+      console.error(err);
+
+      res.status(500).json({ msg: "Something went wrong." });
+    }
+  } else {
+    res.status(403);
+  }
 
   // try get request, if successful return response, otherwise return error message
-  try {
-    const results = await autoSpecies(query);
-
-    return res.status(200).json(results);
-  } catch (err) {
-    console.error(err);
-
-    res.status(500).json({ msg: "Something went wrong." });
-  }
 }
