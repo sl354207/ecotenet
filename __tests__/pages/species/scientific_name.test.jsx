72e76b4f983fc57aeab037bd4232bd3beeb69bb3
sl354207 <sl354207@ohio.edu>
2024-02-07 14:23:38 -0500

    Add rendering tests for species page

--- __tests__/pages/species/scientific_name.test.jsx
+++ __tests__/pages/species/scientific_name.test.jsx
@@ -20,9 +20,20 @@ jest.mock("../../../components/context/UserContext", () => ({
   })),
 }));
 
+const species = {
+  scientific_name: "test scientific name",
+  common_name: "test-common-name",
+  unique_id: ["1", "2"],
+};
+
+const wiki = {
+  title: "test-title",
+  segmentedContent: "test content",
+};
+
 jest.mock("isomorphic-dompurify", () => ({
   DOMPurify: jest.fn(),
-  sanitize: jest.fn(() => "sanitized"),
+  sanitize: jest.fn(() => wiki.segmentedContent),
 }));
 jest.mock("next/router", () => ({
   useRouter: jest.fn(),
@@ -35,27 +46,68 @@ useRouter.mockReturnValue({
   push: pushMock,
 });
 
-const species = {
-  scientific_name: "test-scientific-name",
-  common_name: "test-common-name",
-  unique_id: ["1", "2"],
-};
-
-const wiki = {
-  title: "test-title",
-  segmentedContent: "test content",
-};
-
 describe("Species page", () => {
   describe("rendering", () => {
-    it("should render header and footer", () => {
+    // sanitize.mockReturnValueOnce("test content");
+    it("should render header with common name and footer", () => {
       render(<Species species={species} wiki={wiki} />);
       expect(
         screen.getByRole("heading", {
-          name: /test-scientific-name/i,
+          name: /test scientific name: test-common-name/i,
         })
       ).toBeInTheDocument();
       expect(screen.getByText(/mocked footer/i)).toBeInTheDocument();
     });
+    it("should render header without common name and footer", () => {
+      species.common_name = null;
+      render(<Species species={species} wiki={wiki} />);
+      expect(
+        screen.getByRole("heading", {
+          name: /test scientific name/i,
+        })
+      ).toBeInTheDocument();
+
+      expect(
+        screen.queryByRole("heading", {
+          name: /test-common-name/i,
+        })
+      ).not.toBeInTheDocument();
+      expect(screen.getByText(/mocked footer/i)).toBeInTheDocument();
+    });
+    it("should render wiki source link", () => {
+      render(<Species species={species} wiki={wiki} />);
+      expect(screen.getByRole("link", { name: /wikipedia/i })).toHaveAttribute(
+        "href",
+        "https://en.wikipedia.org/wiki/test_scientific_name?redirect=true"
+      );
+    });
+    it("should render links to ecoregions", () => {
+      render(<Species species={species} wiki={wiki} />);
+
+      expect(screen.getByRole("link", { name: /Eco-1/i })).toHaveAttribute(
+        "href",
+        "/ecoregions/1"
+      );
+
+      expect(screen.getByRole("link", { name: /Eco-2/i })).toHaveAttribute(
+        "href",
+        "/ecoregions/2"
+      );
+    });
+    it("should render wiki content", () => {
+      render(<Species species={species} wiki={wiki} />);
+
+      expect(screen.getByText(/test content/i)).toBeInTheDocument();
+    });
+    it("should render flag button", () => {
+      render(<Species species={species} wiki={wiki} />);
+      expect(screen.getByRole("button", { name: /flag/i })).toBeInTheDocument();
+    });
   });
+  // describe('behavior', () => {
+  //   it('should', () => {
+  //     render(<Species species={species} wiki={wiki} />);
+  //     expect(screen.getByRole('button', { name: /flag/i })).toBeInTheDocument();
+  //   });
+  // });
 });
